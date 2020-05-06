4fab41f4872e49ac4f04c513634e7d38806f7ed0
L200180212 <48323063+L200180212@users.noreply.github.com>
2020-05-06 21:10:21 +0700

    Delete nomer14.py

--- nomer14.py
+++
@@ -1,9 +0,0 @@
-def formatRupiah(bilangan):
-    a = str (bilangan)
-    if len(a) <= 3 :
-         return "Rp " + a
-    else :
-         y = a[-3:]
-         z = a[:-3]
-         return formatRupiah(z) + "." + y
-         print  ("Rp ") + formatRupiah(z) + "." +y
