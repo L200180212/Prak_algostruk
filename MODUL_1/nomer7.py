fc58ff7279a4bf3799a7d159db08be2b1b34de33
L200180212 <48323063+L200180212@users.noreply.github.com>
2020-05-06 21:06:29 +0700

    Create nomer7.py

---
+++ MODUL_1/nomer7.py
@@ -0,0 +1,9 @@
+def faktorPrima(a):
+    bilanganList = []
+    loop=2
+    while loop <= a:
+        if a%loop == 0:
+            a/=loop
+            bilanganList.append(loop)
+        else:
+            loop+=1
